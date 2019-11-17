f5823a13a029618a46478eefe2a6c2b66bdca5a5
Stelios Giakoumidis <stelios.gia.sweden@gmail.com>
2019-11-17 11:11:50 +0100

    Removed untracked files

--- NotificationCommon/Models/Sendout.cs
+++ NotificationCommon/Models/Sendout.cs
@@ -10,9 +10,11 @@ namespace NotificationCommon.Models
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime StartDate { get; set; }
+
         public Enums.RepetitionFrequency RepetitionFrequency { get; set; }
         public Enums.ExecutionTime ExecutionTime { get; set; }
         public Enums.DayOfTheWeek DayOfTheWeek { get; set; }
+
         public DateTime? LastRunAt { get; set; }
         public string Parameters { get; set; }
         public string Username { get; set; }
